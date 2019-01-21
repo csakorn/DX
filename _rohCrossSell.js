a9793601c83871ef77154af1b15c419b5d6e5ae0
Chalermpol Sakorn <chalermpol.sak.no@thaiairways.com>
2019-01-21 14:55:13 +0700

    Update DateSale

--- _rohCrossSell.js
+++ _rohCrossSell.js
@@ -44,7 +44,7 @@ var ROH = {
 
                 ROH.createOBJ();
 
-                if ((check.period(passengerROH.CheckinDate, 3, passengerROH.CheckoutDate, 0) === true) && (check.period(passengerROH.CheckinDate, 30, passengerROH.CheckoutDate, 1) === true)) {
+                if ((check.period(passengerROH.CheckinDate, 4, passengerROH.CheckoutDate, 0) === true) && (check.period(passengerROH.CheckinDate, 30, passengerROH.CheckoutDate, 1) === true)) {
 
                     $.ajax({
                             type: 'POST',
