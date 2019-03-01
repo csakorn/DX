8a26c0208458b17353d50ae287cca3e1d5f7d219
Chalermpol Sakorn <chalermpol.sak.no@thaiairways.com>
2019-03-01 16:32:54 +0700

    update txt insurance

--- _insurance.js
+++ _insurance.js
@@ -17,7 +17,7 @@ let objInsurance = (c, l) => {
             },
             TW: {
                 titlepanel: "\u4fdd\u969c\u60a8\u7684\u65c5\u7a0b",
-                insurance_txt: "<p>\u7d9c\u5408\u65c5\u904a\u4fdd <br>\u5b89\u5fc3\u51fa\u767c\uff0c\u70ba\u60a8\u7684\u65c5\u7a0b\u6dfb\u52a0\u4fdd\u969c </p><ul> <li><i class=\"icon-check title-icon\"></i>\u822a\u73ed\u5ef6\u8aa4,\u822a\u73ed\u53d6\u6d88</li><li><i class=\"icon-check title-icon\"></i>\u907a\u5931\u884c\u674e\u6216\u884c\u674e\u5ef6\u8aa4</li><li><i class=\"icon-check title-icon\"></i>\u9580\u8a3a\x2F\u4f4f\u9662\u91ab\u7642\u8cbb\u7528</li></ul> <p>\u689d\u6b3e\u53ca\u7d30\u5247\u9069\u7528*</p><br><div class=\"box-insurance_detail\"> <div class=\"box-insurance_detail-content\"> <p>\u672c\u4fdd\u96aa\u53ea\u9069\u7528\u65bc\u9999\u6e2f\u5c45\u6c11\u4e26\u5f9e\u9999\u6e2f\u51fa\u767c\u7684\u65c5\u7a0b\u3002\u672c\u65c5\u904a\u4fdd\u96aa\u662f\u7531\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u6709\u9650\u516c\u53f8\u627f\u4fdd\uff08\u4e0b\u7a31\u300c\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u300d\uff09\uff0c\u4e26\u7531\u5b89\u806f\u6551\u63f4\u63d0\u4f9b\u670d\u52d9\u3002 \u5b89\u806f\u6551\u63f4\u70baAllianz Worldwide Partners (Hong Kong) Limited\u7684\u5546\u6a19\uff0c\u70ba\u4e00\u5bb6\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u59d4\u4efb\u7684\u4fdd\u96aa\u4ee3\u7406\u5546\u3002 \u95a3\u4e0b\u8cfc\u8cb7\u7684\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u662f\u7531\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u6709\u9650\u516c\u53f8\u63d0\u4f9b\uff0c\u800c\u4e26\u975e\u7531\u6cf0\u822a\u63d0\u4f9b\u3002\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u70ba\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u7684\u627f\u4fdd\u516c\u53f8\u4e26\u5c07\u8ca0\u8cac\u7e55\u767c\u6709\u95dc\u7684\u4fdd\u55ae\u3002\u6cf0\u822a\u4e26\u975e\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u7684\u4fdd\u96aa\u4ee3\u7406\u4ea6\u4e0d\u662f\u4fdd\u96aa\u92b7\u552e\u7d93\u7d00\u3002\u6cf0\u822a\u4e0d\u6703\u4ee3\u8868\u95a3\u4e0b\u5b89\u6392\u65c5\u904a\u4fdd\u96aa\uff0c\u4ea6\u7121\u9808\u5c0d\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u5c31\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u6240\u63d0\u4f9b\u4e4b\u4efb\u4f55\u8cc7\u6599\u8ca0\u8cac\u3002</p></div></div>",
+                insurance_txt: "<p>\u7d9c\u5408\u65c5\u904a\u4fdd <br>\u5b89\u5fc3\u51fa\u767c\uff0c\u70ba\u60a8\u7684\u65c5\u7a0b\u6dfb\u52a0\u4fdd\u969c </p><ul> <li><i class=\"icon-check title-icon\"></i>\u822a\u73ed\u5ef6\u8aa4,\u822a\u73ed\u53d6\u6d88</li><li><i class=\"icon-check title-icon\"></i>\u907a\u5931\u884c\u674e\u6216\u884c\u674e\u5ef6\u8aa4</li><li><i class=\"icon-check title-icon\"></i>\u9580\u8a3a\x2F\u4f4f\u9662\u91ab\u7642\u8cbb\u7528</li></ul> <p>\u689d\u6b3e\u53ca\u7d30\u5247\u9069\u7528*</p><br><div class=\"box-insurance_detail\"> <div class=\"box-insurance_detail-content\"> <p>\u672c\u4fdd\u96aa\u53ea\u9069\u7528\u65bc\u9999\u6e2f\u5c45\u6c11\u4e26\u5f9e\u9999\u6e2f\u51fa\u767c\u7684\u65c5\u7a0b\u3002\u672c\u65c5\u904a\u4fdd\u96aa\u662f\u7531\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u6709\u9650\u516c\u53f8\u627f\u4fdd\uff08\u4e0b\u7a31\u300c\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u300d\uff09\uff0c\u4e26\u7531\u5b89\u806f\u6551\u63f4\u63d0\u4f9b\u670d\u52d9\u3002 \u5b89\u806f\u6551\u63f4\u70ba Allianz Worldwide Partners (Hong Kong) Limited \u7684\u5546\u6a19\uff0c\u70ba\u4e00\u5bb6\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u59d4\u4efb\u7684\u4fdd\u96aa\u4ee3\u7406\u5546\u3002 \u95a3\u4e0b\u8cfc\u8cb7\u7684\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u662f\u7531\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u6709\u9650\u516c\u53f8\u63d0\u4f9b\uff0c\u800c\u4e26\u975e\u7531\u6cf0\u822a\u63d0\u4f9b\u3002\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u70ba\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u7684\u627f\u4fdd\u516c\u53f8\u4e26\u5c07\u8ca0\u8cac\u7e55\u767c\u6709\u95dc\u7684\u4fdd\u55ae\u3002\u6cf0\u822a\u4e26\u975e\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u7684\u4fdd\u96aa\u4ee3\u7406\u4ea6\u4e0d\u662f\u4fdd\u96aa\u92b7\u552e\u7d93\u7d00\u3002\u6cf0\u822a\u4e0d\u6703\u4ee3\u8868\u95a3\u4e0b\u5b89\u6392\u65c5\u904a\u4fdd\u96aa\uff0c\u4ea6\u7121\u9808\u5c0d\u5229\u5bf6\u570b\u969b\u4fdd\u96aa\u5c31\u300c\u7d9c\u5408\u65c5\u904a\u4fdd\u300d\u6240\u63d0\u4f9b\u4e4b\u4efb\u4f55\u8cc7\u6599\u8ca0\u8cac\u3002</p></div></div>",
                 radio_1: {
                     txt_1: "\u662f\uff0c\u6211\u8981\u70ba\u65c5\u7a0b\u6295\u4fdd\u00a0\u00a0 \u4ee5\u4fdd\u969c\u6bcf\u4e00\u4f4d\u65c5\u5ba2",
                     txt_2: " ",
@@ -69,7 +69,7 @@ let objInsurance = (c, l) => {
                 radio_1: {
                     txt_1: "Yes, I want to protect my trip",
                     txt_2: "for all passengers",
-                    linkPolicy: "http://www.magroup-online.com/TGA/TH/EN/TG_TCs_Domestic&Outbound_EN.pdf",
+                    linkPolicy: "https://www.magroup-online.com/TGA/TH/EN/TG_TCs_Domestic&Outbound_EN.pdf",
                     txtPolicy: "For Policy Wording"
                 },
                 radio_2: {
@@ -96,10 +96,10 @@ let objInsurance = (c, l) => {
     return _objInsurance[c][l];
 }
 // var addinsurance = function(xcountry){
-// 	let langx = "";
-// 	for(x in _objInsurance){
-// 		if(x == xcountry){
-// 			langx = [x,eBaDataLayer.language]
+//  let langx = "";
+//  for(x in _objInsurance){
+//      if(x == xcountry){
+//          langx = [x,eBaDataLayer.language]
 //         }
 //     }
 // return langx;
@@ -116,8 +116,47 @@ function insurance(xcountry, xlanguage) {
             $('label[for="tpl3_widget-input-purchaseForm-insuranceForm-insuranceRadioGroup-insuranceRadioGroup1"]>a').html(lc.radio_1.txtPolicy)
             $('label[for="tpl3_widget-input-purchaseForm-insuranceForm-insuranceRadioGroup-insuranceRadioGroup1"]>a').attr('href', lc.radio_1.linkPolicy);
             $('label[for="tpl3_widget-input-purchaseForm-insuranceForm-insuranceRadioGroup-insuranceRadioGroup2"]').html(lc.radio_2.txt_1 + " " + lc.radio_2.txt_2);
-        } else {
-            return false;
         }
     }, 1000, 5);
+
+    let lcx = (objInsurance(xcountry, xlanguage) != undefined) ? objInsurance(xcountry, xlanguage) : objInsurance(xcountry, 'GB')
+    implibdx.core.updateDom("#terms-and-conditions", function() {
+
+        let insurances_tc = $('.checkbox a')[4];
+
+        if(insurances_tc.textContent  === 'insurance conditions Open new window' && eBaDataLayer.bound[0].dep_country === "TH"){
+            insurances_tc.href = lcx.radio_1.linkPolicy;
+        }else if($('.checkbox a')[4].textContent === 'insurance conditions Open new window' && eBaDataLayer.bound[0].dep_country !== "TH"){
+            insurances_tc.remove()
+        }
+        
+
+    //     for (x in $('.checkbox a')) {
+    //         if ($('.checkbox a')[x].textContent === 'insurance conditions Open new window' && eBaDataLayer.bound[0].dep_country === "TH") {
+    //            $('.checkbox a')[x].href = lcx.radio_1.linkPolicy;
+
+    //        }else if($('.checkbox a')[x].textContent === 'insurance conditions Open new window'){               
+    //         $('.checkbox a')[x].remove()
+    //     }
+    // }
+   }, 1000, 8);
+
+    // if (eBaDataLayer.currency === 'THB') {
+
+    //     implibdx.core.updateDom("#widget-group-purchaseForm-termsConditionsForm-termsAndCondition", function() {
+    //         for (x in $('.checkbox a')) {
+    //             if ($('.checkbox a')[x].textContent == 'insurance conditions Open new window') {
+    //                 $('.checkbox a')[x].href = 'https://www.magroup-online.com/TGA/TH/EN/TG_TCs_Domestic&Outbound_EN.pdf'
+    //             }
+    //         }
+    //     }, 1000, 5);
+    // }else{
+    //     implibdx.core.updateDom("#widget-group-purchaseForm-termsConditionsForm-termsAndCondition", function() {
+    //         for (x in $('.checkbox a')) {
+    //             if ($('.checkbox a')[x].textContent == 'insurance conditions Open new window') {
+    //                 $('.checkbox a')[x].remove()
+    //             }
+    //         }
+    //     }, 1000, 5);
+    // }
 }
